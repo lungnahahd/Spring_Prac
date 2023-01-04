75de4ad00464a302e64a305d34c45188ce84a904
lungnahahd <jyoungjk33@gmail.com>
2023-01-04 18:16:08 +0900

    feat : 로그인 쿠키 구현 쿠키 생성 및 삭제

--- FirstSpring/src/main/java/com/lungnaha/app/LoginController.java
+++ FirstSpring/src/main/java/com/lungnaha/app/LoginController.java
@@ -1,7 +1,9 @@
 package com.lungnaha.app;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +19,21 @@ public class LoginController {
 	}
 	
 	@PostMapping("/login")
-	public String login(String id, String pwd, String rememberId) throws Exception {
+	public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws Exception {
 		// id와 pwd를 확인
 		if(!loginCheck(id,pwd)) {
 			String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.","utf-8");
 			return "redirect:/login/login?msg=" + msg;
 		}
+		Cookie cookie = new Cookie("id",id); // 쿠키 생성
+		if(rememberId) {
+			cookie.setMaxAge(60*60*24); // 유효기간 지정
+		}else {
+			cookie.setMaxAge(0);
+		}
+		response.addCookie(cookie);
+		
+
 		return "redirect:/";
 	}
 
