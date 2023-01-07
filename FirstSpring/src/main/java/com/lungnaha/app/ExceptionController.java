d8aaf0977b72b3ca61d0ae1c0861e5f5a01201a0
lungnahahd <jyoungjk33@gmail.com>
2023-01-07 16:51:49 +0900

    fix : 에러 메서드 발생 코드 변경 200 -> 500 으로 상태 코드 변경 구현

--- FirstSpring/src/main/java/com/lungnaha/app/ExceptionController.java
+++ FirstSpring/src/main/java/com/lungnaha/app/ExceptionController.java
@@ -1,14 +1,17 @@
 package com.lungnaha.app;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 //@ControllerAdvice 어노테이션을 사용하면 해당 컨트롤러가 아니라도 어디에서든 @ExceptionHandler 호출 가
 public class ExceptionController {
 	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 해당 어노테이션으로 상태코드를 200 -> 500 으로 변경 
 	public String catcher(Exception ex, Model m) {
 		m.addAttribute("ex",ex); // view 에서 보여주기 위한 정보를 model을 통해서 넘겨주
 		return "error";
